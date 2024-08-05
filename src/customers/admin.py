6c348c5aff8b6be72c0a0ab09f82b228beed9815
Pedram <pedisdi@gmail.com>
2024-08-05 14:29:59 +0330

    feat: add 'customer' model

--- src/customers/admin.py
+++ src/customers/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from .models import Customer
+from django.utils.translation import gettext_lazy as _
+from accounts.admin import CustomUserAdmin
 
-# Register your models here.
+
+class CustomerAdmin(CustomUserAdmin):
+    model = Customer
+    fieldsets = (
+        (None, {"fields": ("email", "phone", "password")}),
+        (_("Personal info"), {
+            "fields": ("first_name", "last_name", "date_of_birth", "national_id",
+                       "gender", "image")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_deleted",
+                    "groups",
+                    "user_permissions",
+                    # "addresses" # todo: add new addresses in user admin change page
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", 'date_joined', 'date_modified')}),
+        (_("financial details"), {"fields": ("balance",)})
+    )
+
+
+admin.site.register(Customer, CustomerAdmin)
--- src/customers/models.py
+++ src/customers/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+User = get_user_model()
+
+
+class Customer(User):
+    balance = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = _("مشتری")
+        verbose_name_plural = _("مشتریان")
+
+    def save(self, *args, **kwargs):
+        self.is_staff = False
+        self.is_superuser = False
+        super().save(*args, **kwargs)

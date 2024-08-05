e5a5e9a2d4dc4b7b9aac7dbe00e01e523514a58a
Pedram <pedisdi@gmail.com>
2024-08-05 19:14:56 +0330

    feat: Add "Store" and "Staff" models

--- src/vendors/admin.py
+++ src/vendors/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from .models import Staff, Store
+from django.utils.translation import gettext_lazy as _
+from accounts.admin import CustomUserAdmin
 
-# Register your models here.
+
+@admin.register(Staff)
+class StaffAdmin(CustomUserAdmin):
+    model = Staff
+    fieldsets = (
+        (_("اطلاعات حساب کاربری"), {"fields": ("email", "phone", "password")}),
+        (_("اطلاعات شخصی"), {
+            "fields": ("first_name", "last_name", "date_of_birth", "national_id",
+                       "gender", "image")}),
+        (
+            _("دسترسی ها"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "is_deleted",
+                    "groups",
+                    "user_permissions",
+                    # "addresses" # todo: add new addresses in user admin change page
+                ),
+            },
+        ),
+        (_("تاریخ های مهم"), {"fields": ("last_login", 'date_joined', 'date_modified')}),
+        (_("اطلاعات کارمند"), {"fields": ("role", "store")})
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2", "store"),
+            },
+        ),
+    )
+
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    fields = ['name', 'description', 'address', 'order_count', 'slug']
+    readonly_fields = ['order_count', 'slug']
--- src/vendors/models.py
+++ src/vendors/models.py
@@ -1,3 +1,57 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.text import slugify
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from website.models import Address, CreateUpdateDateTimeFieldMixin
+
+User = get_user_model()
+
+
+class Store(CreateUpdateDateTimeFieldMixin, models.Model):
+    name = models.CharField(_("نام فروشگاه"), max_length=100, unique=True)
+    slug = models.SlugField(max_length=150, unique=True, blank=True, allow_unicode=True)
+    description = models.TextField(verbose_name=_("توضیحات"), null=True, blank=True)
+    address = models.OneToOneField(
+        Address,
+        on_delete=models.SET_NULL,
+        related_name="store",
+        verbose_name=_('آدرس'),
+        null=True,
+        blank=True
+    )
+    order_count = models.PositiveIntegerField(verbose_name=_("تعداد کل سفارشات"), default=0)
+
+    def save(self, *args, **kwargs):
+        self.set_slug()
+        super(Store, self).save(*args, **kwargs)
+
+    def set_slug(self):
+        if not self.id or self.slug != slugify(self.name, allow_unicode=True):
+            self.slug = slugify(self.name, allow_unicode=True)
+
+    class Meta:
+        verbose_name = _("فروشگاه")
+        verbose_name_plural = _("فروشگاه ها")
+
+    def __str__(self):
+        return self.name
+
+
+class Staff(User):
+    class Roles(models.TextChoices):
+        OWNER = "Owner", _("مدیر فروشگاه")
+        MANAGER = "Manager", _("مدیر محصول")
+        OPERATOR = "Operator", _("ناظر")
+
+    role = models.CharField(_("سمت"), max_length=12, choices=Roles.choices, default=Roles.OPERATOR)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, verbose_name=_("فروشگاه"), related_name='staff')
+
+    class Meta:
+        verbose_name = _("کارمند")
+        verbose_name_plural = _("کارمندان")
+
+    def save(self, *args, **kwargs):
+        self.is_staff = True
+        self.is_superuser = False
+        super().save(*args, **kwargs)

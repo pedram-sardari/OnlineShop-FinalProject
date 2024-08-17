5c836952ad8ae45ea6189cb15d94fafa60f53be4
Pedram <pedisdi@gmail.com>
2024-08-17 12:31:25 +0330

    feat: Add `PhoneForm`, `LoginPhoneForm`, `RegisterPhoneForm`, and `OTPForm`

--- src/accounts/forms.py
+++ src/accounts/forms.py
@@ -1,9 +1,12 @@
 from django import forms
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 
 from accounts.models import UserAddress
 from website.forms import FormatFormFieldsMixin
+from website.validators import phone_validator
 
 User = get_user_model()
 
@@ -36,16 +39,48 @@ class UserAddressForm(FormatFormFieldsMixin, forms.ModelForm):
 
 
 class PhoneForm(FormatFormFieldsMixin, forms.Form):
-    phone = forms.CharField(max_length=11)
+    phone = forms.CharField(max_length=11, label=_("شماره تماس"), validators=[phone_validator])
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.format_fields()
 
 
-class VerificationCodeForm(FormatFormFieldsMixin, forms.Form):
-    verification_code = forms.CharField(max_length=10)
+class LoginPhoneForm(PhoneForm):
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        # Try to retrieve the user associated with the session phone number
+        if not User.objects.filter(phone=phone).exists():
+            raise forms.ValidationError('شما ثبتنام نکرده اید')
+        return phone
+
+
+class RegisterPhoneForm(PhoneForm):
+    def clean_phone(self):
+        phone = self.cleaned_data.get('phone')
+        if User.objects.filter(phone=phone).exists():
+            raise forms.ValidationError('این شماره تماس قبلا ثبت شده است.')
+        return phone
+
+
+class OTPForm(FormatFormFieldsMixin, forms.Form):
+    otp = forms.CharField(max_length=settings.OTP_LENGTH, label="کد تایید")
 
     def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request', None)
         super().__init__(*args, **kwargs)
         self.format_fields()
+
+    def clean_otp(self):
+        user_otp = self.cleaned_data['otp']
+        session_otp = self.request.session.get('otp')
+        print('*' * 50, user_otp, session_otp)
+
+        # If OTP session is missing or expired, redirect back to login
+        if not session_otp:
+            raise forms.ValidationError('کد تایید منقضی شده است')
+
+        # If the provided OTP does not match the session OTP
+        if user_otp != session_otp:
+            raise forms.ValidationError('کد تایید اشتباه است')
+        return user_otp

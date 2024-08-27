35a46ff085e9ed92bca6eea3fc0b73ba5d8e6f77
Pedram <pedisdi@gmail.com>
2024-08-27 22:13:37 +0330

    feat: change the structure of the serialized data using the `get_paginated_response` method of the `MyPagination` class

--- src/website/paginations.py
+++ src/website/paginations.py
@@ -1,7 +1,20 @@
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class MyPagination(PageNumberPagination):
     page_size = 2
     page_size_query_param = 'page_size'
     max_page_size = 10000
+
+    def get_paginated_response(self, data):
+        return Response({
+            'links': {
+                'next': self.get_next_link(),
+                'previous': self.get_previous_link()
+            },
+            'count': self.page.paginator.count,
+            'num_pages': self.page.paginator.num_pages,
+            'current_page': self.page.number,
+            'results': data
+        })

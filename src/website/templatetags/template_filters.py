018a6618d01acf57f203aa6d199f793c6aea3bf1
Pedram Sardari <pedisdi@gmail.com>
2024-09-06 20:35:13 +0330

    fix: correct some i18n issues

--- src/website/templatetags/template_filters.py
+++ src/website/templatetags/template_filters.py
@@ -1,4 +1,5 @@
 from django import template
+from babel.numbers import format_decimal
 
 register = template.Library()
 
@@ -17,3 +18,12 @@ def slice_string(value, length=15):
     if len(value) > length:
         return value[:length - 3] + '...'
     return value
+
+
+@register.filter(name='format_number')
+def format_number(value, locale='fa'):
+    """Format a number according to the locale."""
+    try:
+        return format_decimal(value, locale=locale)
+    except (TypeError, ValueError):
+        return value
